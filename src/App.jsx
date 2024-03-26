eead183a97057404529063d6c6b7a98e1d2b0291
tapsikk <tapsgame@gmail.com>
2024-03-26 20:07:09 +0200

    endD

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // import { useEffect, useState } from "react";
-import Description from "./components/Description/Description";
 // import Feedback from "./components/Feedback/Feedback";
 // import Options from "./components/Options/Options";
 // import Notification from "./components/Notification/Notification";
+import Description from "./components/Description/Description";
 
 // const App = () => {
 //   const [feedbacks, setFeedbacks] = useState(
