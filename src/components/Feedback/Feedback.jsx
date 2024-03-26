efc0f26d66c6362d6be38295520549492808cea2
tapsikk <tapsgame@gmail.com>
2024-03-26 15:21:12 +0200

    %%%

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,13 @@
 const Feedback = ({ marks }) => {
   const total = marks.good + marks.neutral + marks.bad;
+  const positive = Math.round((marks.good / total) * 100)
   return (
     <div>
       <p>Good: {marks.good}</p>
       <p>Neutral: {marks.neutral}</p>
       <p>Bad: {marks.bad}</p>
       <p>Total: {total}</p>
+      <p>Positive: {positive}%</p>
     </div>
   );
 };
