3bbb522168551a7b9b7e507b48a8dccc729f60f9
tapsikk <tapsgame@gmail.com>
2024-03-26 17:13:48 +0200

    rell

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ const App = () => {
   const [total, setTotal] = useState(0);
   const [positive, setPositive] = useState(0);
 
-
   useEffect(() => {
     setTotal(feedbacks.good + feedbacks.neutral + feedbacks.bad);
     localStorage.setItem("marks", JSON.stringify(feedbacks));
@@ -24,8 +23,7 @@ const App = () => {
 
   useEffect(() => {
     // формулу зробив як на вiдео, в теорiї була iнша :)
-    if (total)
-    setPositive(Math.round(((feedbacks.good) / total) * 100));
+    if (total) setPositive(Math.round((feedbacks.good / total) * 100));
   }, [total]);
 
   const updateFeedback = (feedbackType) => {
@@ -45,7 +43,11 @@ const App = () => {
   return (
     <>
       <Description />
-      <Options updateF={updateFeedback} total={total} resetFeedbacks={resetFeedbacks} />
+      <Options
+        updateF={updateFeedback}
+        total={total}
+        resetFeedbacks={resetFeedbacks}
+      />
       {/* зменшити при тотал */}
       {total ? (
         <Feedback marks={feedbacks} total={total} positive={positive} />
