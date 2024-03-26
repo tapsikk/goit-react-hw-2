2dfcaa1e0e599dc9b14214a1339681c71989f167
tapsikk <tapsgame@gmail.com>
2024-03-26 19:51:46 +0200

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useEffect, useState } from "react";
-// import Description from "./components/Description/Description";
+import Description from "./components/Description/Description";
 // import Feedback from "./components/Feedback/Feedback";
 // import Options from "./components/Options/Options";
 // import Notification from "./components/Notification/Notification";
