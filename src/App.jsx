b188cbe2a37ab2b0abaac2d86da9fed0a4e9c6ec
tapsikk <tapsgame@gmail.com>
2024-03-26 20:20:47 +0200

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,56 @@
-// import { useEffect, useState } from "react";
-// import Feedback from "./components/Feedback/Feedback";
-// import Options from "./components/Options/Options";
-// import Notification from "./components/Notification/Notification";
-import Description from "./components/Description/Description";
+import { useEffect, useState } from "react";
+import Feedback from "./Feedback/Feedback";
+import Options from "./Options/Options";
+import Notification from "./Notification/Notification";
+import Description from "./Description/Description";
 
-// const App = () => {
-//   const [feedbacks, setFeedbacks] = useState(
-//     JSON.parse(localStorage.getItem("marks")) || {
-//       good: 0,
-//       neutral: 0,
-//       bad: 0,
-//     }
-//   );
+const App = () => {
+  const [feedbacks, setFeedbacks] = useState(
+    JSON.parse(localStorage.getItem("marks")) || {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    }
+  );
 
-//   useEffect(() => {
-//     localStorage.setItem("marks", JSON.stringify(feedbacks));
-//   }, [feedbacks]);
+  useEffect(() => {
+    localStorage.setItem("marks", JSON.stringify(feedbacks));
+  }, [feedbacks]);
 
-//   const updateFeedback = (feedbackType) => {
-//     setFeedbacks({
-//       ...feedbacks,
-//       [feedbackType]: feedbacks[feedbackType] + 1,
-//     });
-//   };
-//   const resetFeedbacks = () => {
-//     setFeedbacks({
-//       good: 0,
-//       neutral: 0,
-//       bad: 0,
-//     });
-//   };
-//   const total = feedbacks.good + feedbacks.neutral + feedbacks.bad;
-//   const positivePercentage = total > 0 ? Math.round((feedbacks.good / total) * 100) : 0;
-//   return (
-//     <>
-//       <Description />
-//       <Options
-//         isVisible={total > 0}
-//         updateF={updateFeedback}
-//         resetFeedbacks={resetFeedbacks}
-//       />
-//       {total > 0 ? (
-//         <Feedback
-//           marks={feedbacks}
-//           total={total}
-//           postive={positivePercentage}
-//         />
-//       ) : (
-//         <Notification />
-//       )}
-//     </>
-//   );
-// };
+  const updateFeedback = (feedbackType) => {
+    setFeedbacks({
+      ...feedbacks,
+      [feedbackType]: feedbacks[feedbackType] + 1,
+    });
+  };
+  const resetFeedbacks = () => {
+    setFeedbacks({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+  const total = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+  const positivePercentage = total > 0 ? Math.round((feedbacks.good / total) * 100) : 0;
+  return (
+    <>
+      <Description />
+      <Options
+        isVisible={total > 0}
+        updateF={updateFeedback}
+        resetFeedbacks={resetFeedbacks}
+      />
+      {total > 0 ? (
+        <Feedback
+          marks={feedbacks}
+          total={total}
+          postive={positivePercentage}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+};
 
 export default App;
